a379b8d02c2909a338bc1716ebfc99136cb05109
Jitendra Panwar <gm.jitendra@gmail.com>
2024-05-25 21:07:42 +0530

    actions

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to my first Docker Node App")
 })
 
-app.listen(3000, function() {
+app.get("/ga", (req, res) => {
+  res.send("Implemented github actions")
+})
+
+app.listen(3000, function () {
   console.log("app running on port: 3000 ")
 })
